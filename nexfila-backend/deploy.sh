a3229978e3908531df0d2070267b8c996505f273
AscendusEduTec <ascendusthiago@gmail.com>
2026-06-07 13:22:41 -0300

    gitpull..

--- nexfila-backend/deploy.sh
+++ nexfila-backend/deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /opt/Nexfila/nexfila-frontend && git pull
-cd /opt/Nexfila/nexfila-backend && git pull
+cd /opt/Nexfila && git pull
 cd /opt/Nexfila/nexfila-backend && docker compose down && docker compose up -d --build
